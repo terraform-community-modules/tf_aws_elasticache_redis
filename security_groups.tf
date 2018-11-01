a17ea6666389685a7434062af7314d9b70ee80c7
Chris Wright <chris@dxw.com>
2018-11-01 15:41:08 +0000

    (Fix) Terraform formatting
    * Reformat so that `terraform fmt -check` passes

--- security_groups.tf
+++ security_groups.tf
@@ -19,10 +19,10 @@ resource "aws_security_group_rule" "redis_ingress" {
 }
 
 resource "aws_security_group_rule" "redis_networks_ingress" {
-  type                     = "ingress"
-  from_port                = "${var.redis_port}"
-  to_port                  = "${var.redis_port}"
-  protocol                 = "tcp"
-  cidr_blocks              = ["${var.allowed_cidr}"]
-  security_group_id        = "${aws_security_group.redis_security_group.id}"
+  type              = "ingress"
+  from_port         = "${var.redis_port}"
+  to_port           = "${var.redis_port}"
+  protocol          = "tcp"
+  cidr_blocks       = ["${var.allowed_cidr}"]
+  security_group_id = "${aws_security_group.redis_security_group.id}"
 }
