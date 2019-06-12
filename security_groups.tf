2957e1f94d41651445fecc97479f75d812273648
Mohsin Hijazee <mshafique@ekomi-group.com>
2019-06-12 11:45:58 +0200

    Fixed syntax error for CIDR blocks.

--- security_groups.tf
+++ security_groups.tf
@@ -23,6 +23,6 @@ resource "aws_security_group_rule" "redis_networks_ingress" {
   from_port         = "${var.redis_port}"
   to_port           = "${var.redis_port}"
   protocol          = "tcp"
-  cidr_blocks       = ["${var.allowed_cidr}"]
+  cidr_blocks       = "${var.allowed_cidr}"
   security_group_id = "${aws_security_group.redis_security_group.id}"
 }
